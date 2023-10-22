303bc5f1d478a48d31e4874724d9d0635318d681
Ankit Choubey <imkitchoubey@gmail.com>
2023-10-22 17:34:47 +0530

    UI of Website is created

---
+++ index.js
@@ -0,0 +1,26 @@
+// About Section 
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname){
+    for(tablink of tabLinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabContents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Sidebar Menu Function 
+
+let sideMenu = document.getElementById("side-menu");
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-35vw";
+}
