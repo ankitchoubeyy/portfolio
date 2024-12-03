59cdbd2f6fbe43bb98cbfd33ca719451326f51d0
ankitchoubeyy <imkitchoubey@gmail.com>
2024-12-03 13:54:09 +0530

    about us page content added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { FaBookReader, FaLaptopCode, FaBook } from "react-icons/fa";
+import { MdDoubleArrow, MdSportsCricket } from "react-icons/md";
+import { RiEmotionHappyFill } from "react-icons/ri";
 
 const page = () => {
   return (
-    <div className="mt-28 container mx-auto px-5 min-h-screen text-justify">
-      <h1 className="text-xl md:text-4xl font-bold my-10 font-Secondary">
+    <div className="mt-20 container mx-auto px-5 min-h-screen">
+      <h1 className="text-md md:text-3xl font-bold my-5 font-Secondary">
         Hi, I&apos;m Ankit Choubey
       </h1>
-      <div className="flex flex-col gap-5 md:gap-10 font-Tertiary leading-relaxed text-gray-300">
+      <div className="flex flex-col gap-5 md:gap-10 font-Tertiary leading-loose text-gray-300 text-sm ">
         <p>
           I&apos;m a passionate{" "}
           <span className="underline underline-orange">
@@ -33,19 +36,19 @@ const page = () => {
           cricket, or exploring ways to make everyday workflows more efficient.
         </p>
       </div>
-      <div className="bg-[#181818] px-5 pt-4 pb-12 rounded-3xl mt-10">
-        <h1 className="text-xl md:text-4xl font-bold my-10 font-Secondary">
+      <div className="bg-[#181818] px-5 pt-4 pb-12 rounded-3xl mt-10 ">
+        <h1 className="md:text-4xl font-bold my-10 font-Secondary">
           How My Expertise Can Benefit You?
         </h1>
-        <div className="flex flex-col gap-5 md:gap-10 font-Tertiary leading-relaxed text-gray-300 text-justify">
-          <p>
+        <div className="flex flex-col gap-5 md:gap-10 font-Tertiary leading-loose text-gray-300 text-sm">
+          <p className="text-sm leading-loose">
             As a seasoned Full Stack Developer with proficiency in the MERN
             stack, Python, and Django, I offer a comprehensive skill set to
             elevate your projects. Here&apos;s how my expertise can benefit you:
           </p>
           <ul
             className="list-disc ml-5 flex
-        flex-col gap-2 text-justify"
+        flex-col gap-2"
           >
             <li>
               <span className="underline underline-orange font-bold">
@@ -55,22 +58,101 @@ const page = () => {
               seamless integration and a cohesive user experience.
             </li>
             <li>
-              <span className="underline underline-orange font-bold">Rapid Development:</span> My ability to work across the entire stack
-              accelerates the development process, enabling faster
-              time-to-market for your products.
+              <span className="underline underline-orange font-bold">
+                Rapid Development:
+              </span>{" "}
+              My ability to work across the entire stack accelerates the
+              development process, enabling faster time-to-market for your
+              products.
             </li>
             <li>
-              <span className="underline underline-orange font-bold">Scalability and Performance:</span> I design solutions that are both
-              scalable and high-performing, ensuring your applications can grow
-              and handle increased user demand.
+              <span className="underline underline-orange font-bold">
+                Scalability and Performance:
+              </span>{" "}
+              I design solutions that are both scalable and high-performing,
+              ensuring your applications can grow and handle increased user
+              demand.
             </li>
             <li>
-              <span className="underline underline-orange font-bold">Continuous Support:</span> Beyond development, I offer ongoing
-              maintenance and support to keep your applications running smoothly
-              and up-to-date.
+              <span className="underline underline-orange font-bold">
+                Continuous Support:
+              </span>{" "}
+              Beyond development, I offer ongoing maintenance and support to
+              keep your applications running smoothly and up-to-date.
             </li>
           </ul>
         </div>
+      </div>
+      <div>
+        <h1 className="text-md flex gap-2 md:text-3xl font-bold mb-5 mt-10 font-Secondary">
+          <FaBookReader className="text-xl md:text-3xl" />{" "}
+          <span>Education</span>
+        </h1>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold">
+            Bachelor of Technology (B.Tech)
+          </span>
+          <span className="font-Quaternary leading-loose flex items-center gap-2">
+            <MdDoubleArrow />
+            MIET, Greater Noida{" "}
+            <span className="text-orange-300 text-xs">2021-2025</span>
+          </span>
+        </p>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold">
+            Higher Secondary Education (12th Grade)
+          </span>
+          <span className="font-Quaternary flex items-center gap-2">
+            <MdDoubleArrow />
+            Shaiffali Public School, Greater Noida
+            <span className="text-orange-300 text-xs">2020-2021</span>
+          </span>
+        </p>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold">
+            Secondary Education (10th Grade)
+          </span>
+          <span className="font-Quaternary flex items-center gap-2">
+            <MdDoubleArrow />
+            Shaiffali Public School, Greater Noida
+            <span className="text-orange-300 text-xs">2018-2019</span>
+          </span>
+        </p>
+      </div>
+      {/* Hobbies section */}
+      <div>
+        <h1 className="text-md flex gap-2 md:text-3xl font-bold mb-5 mt-10 font-Secondary">
+          My Hoobies
+        </h1>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold flex items-center gap-2">
+            <FaLaptopCode />
+            Exploring the World of Coding
+          </span>
+          <span className="font-Quaternary leading-loose ">
+            Embracing innovation and diving deep into the tech universe. Every
+            click is a new adventure.
+          </span>
+        </p>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold flex items-center gap-2">
+            <FaBook />
+            Reading
+          </span>
+          <span className="font-Quaternary flex items-center gap-2">
+            Escaping into worlds beyond imagination, one page at a time.
+          </span>
+        </p>
+        <p className="flex flex-col gap-2 mb-4">
+          <span className="underline underline-orange font-bold flex items-center gap-2">
+            <MdSportsCricket />
+            Cricket
+          </span>
+          <span className="font-Quaternary flex items-center gap-2">
+            From the crease to the stands, living for the sound of the bat
+            hitting the ball. Cricket is life.
+          </span>
+        </p>
       </div>
     </div>
   );
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,7 @@ import { FaArrowCircleDown } from "react-icons/fa";
 const Resume = () => {
   return (
     <>
-      <div className="flex items-center justify-center bg-[#ee8b34] hover:bg-[#ec6313] transition-all duration-300 px-4 py-2 rounded-2xl gap-2 font-Secondary animate-color-change">
+      <div className="flex items-center justify-center bg-[#ee8b34] hover:bg-[#ec6313] transition-all duration-300 px-4 py-2 rounded-2xl gap-2 font-Secondary ">
         <FaArrowCircleDown />
         <a href="/Ankit_Choubey_Resume.pdf" download={true}>
           Resume
